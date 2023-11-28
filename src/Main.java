89a07b794f4404a5d1d36c87dec9eb3982d0f2ae
Misu099 <mihaialexvp@gmail.com>
2023-11-28 23:13:58 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        MyArrayList<String> test = new MyArrayList<>(4);
+        test.add("asd",0);
+        System.out.println(test);
+        test.printIt();
+
+
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,22 @@
-public class MyArrayList {
+public class MyArrayList <T>{
+
+    private Object[] baza;
+    private int size;
+
+
+    public MyArrayList(int size){
+        baza = new Object[size];
+    }
+
+public void add(T item, int index){
+        baza[index] = item;
+}
+
+public void printIt(){
+    System.out.print("Baza are: ");
+    for (Object o : baza) {
+        System.out.print(o + ", ");
+    }
+}
+
 }
