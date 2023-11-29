ebe4293bc5720c0ff340128427e91b7bba4edc01
Misu099 <mihaialexvp@gmail.com>
2023-11-29 13:31:56 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
 
-        MyArrayList<String> test = new MyArrayList<>(4);
-        test.add("asd",0);
-        System.out.println(test);
+        MyArrayList<String> test = new MyArrayList<>(5);
+        System.out.println(test.isEmpty());
+        test.add("asd");
+        System.out.println(test.isEmpty());
+        test.add("a");
+        test.add("5");
+
         test.printIt();
+        test.remove("5");
+        test.add("555");
+        test.add("b");
+        test.add("g");
+        test.add("b");
+        test.add("g");
+        test.printIt();
+
 
 
     }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,22 +1,104 @@
-public class MyArrayList <T>{
+import java.util.Arrays;
 
+public class MyArrayList<T> {
+
+
+    private final int DEFAULT_SIZE = 10;
     private Object[] baza;
-    private int size;
+    private Object[] helper;
+    private int size = 0;
+    private int nextPos;
 
 
-    public MyArrayList(int size){
+    public MyArrayList() {
+        baza = new Object[DEFAULT_SIZE];
+        size = DEFAULT_SIZE;
+    }
+
+    public MyArrayList(int size) {
+        this.size = size;
         baza = new Object[size];
     }
 
-public void add(T item, int index){
-        baza[index] = item;
-}
-
-public void printIt(){
-    System.out.print("Baza are: ");
-    for (Object o : baza) {
-        System.out.print(o + ", ");
+    public void add(T obj) {
+        if (nextPos >= baza.length - 1) {
+            setSize(baza.length + 1);
+        }
+        while (true) {
+            if (baza[nextPos] == null) {
+                baza[nextPos] = obj;
+                break;
+            }
+            nextPos++;
+        }
     }
-}
+
+    public void add(T obj, int index) {
+    if( index < baza.length){
+        baza[index] = obj; //Overwrites already existing obj, doesn't care if is empty or not.
+    }
+
+    }
+
+
+    public boolean isEmpty() {
+        for (int i = 0; i < baza.length; i++) {
+            if (baza[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void remove(int index) {
+        if (baza[index] == null) {
+            System.out.println("Pos: " + index + " is already empty.");
+        } else {
+
+            System.out.println("Removed item: " + baza[index] + " at pos: " + index + ".");
+            baza[index] = null;
+        }
+    }
+
+
+    public void remove(T obj) {
+        for (int i = 0; i < baza.length; i++) {
+            if (baza[i] != null && baza[i].equals(obj)) {
+                baza[i] = null;
+                System.out.println("Removed item: " + obj.toString() + " at pos: " + i + ".");
+                break;
+            }
+            if (i == baza.length - 1) {
+                System.out.println("Item: " + obj.toString() + " is not present!");
+            }
+        }
+
+    }
+
+    public int getSize() {
+        return baza.length;
+    }
+
+    public void setSize(int size) {
+        baza = Arrays.copyOf(baza, size);
+        int losingItems = this.size - size;
+        if (losingItems > 0) {
+            System.out.println("Lost: " + losingItems + " items");
+        }
+        this.size = size;
+    }
+
+
+    public void printIt() {
+        System.out.print("Baza are: ");
+        for (int i = 0; i < baza.length; i++) {
+            if (i == baza.length - 1) {
+                System.out.print(baza[i] + ".");
+                break;
+            }
+            System.out.print(baza[i] + ", ");
+        }
+        System.out.println();
+    }
 
 }
