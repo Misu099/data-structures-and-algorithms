a93dc4b356640d21b6d10ef36ce66e466669eb04
Robert24M <95437333+Robert24M@users.noreply.github.com>
2023-11-30 22:41:20 +0200

    feat: add todos

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -2,9 +2,8 @@ import java.util.Arrays;
 
 public class MyArrayList<T> {
 
-
     private final int DEFAULT_SIZE = 10;
-    private Object[] baza;
+    private Object[] baza; //todo: only english
     private Object[] helper;
     private int size = 0;
     private int nextPos;
@@ -34,9 +33,9 @@ public class MyArrayList<T> {
     }
 
     public void add(T obj, int index) {
-    if(index > -1 && index < baza.length){
-        baza[index] = obj; //Overwrites already existing obj, doesn't care if is empty or not.
-    }
+        if (index > -1 && index < baza.length) {
+            baza[index] = obj; //Overwrites already existing obj, doesn't care if is empty or not.
+        }
 
     }
 
@@ -51,7 +50,7 @@ public class MyArrayList<T> {
     }
 
     public void remove(int index) {
-        if(index > -1 && index < baza.length) {
+        if (index > -1 && index < baza.length) {
             baza[index] = null;
         }
     }
@@ -61,7 +60,7 @@ public class MyArrayList<T> {
         for (int i = 0; i < baza.length; i++) {
             if (baza[i] != null && baza[i].equals(obj)) {
                 baza[i] = null;
-                System.out.println("Removed item: " + obj.toString() + " at pos: " + i + ".");
+                System.out.println("Removed item: " + obj.toString() + " at pos: " + i + "."); //obj.toString will not return what you probably expect
                 break;
             }
             if (i == baza.length - 1) {
@@ -75,18 +74,19 @@ public class MyArrayList<T> {
         return baza.length;
     }
 
+    //todo: this method should be rather private and be used just to change the size of array when needed
     public void setSize(int size) {
         baza = Arrays.copyOf(baza, size);
-        int losingItems = this.size - size;
+        int losingItems = this.size - size; //bad naming
         if (losingItems > 0) {
             System.out.println("Lost: " + losingItems + " items");
         }
         this.size = size;
     }
 
-
+    //todo: ugly naming(personal opinion). This may be a toString implementation(read about it or ask me).
     public void printIt() {
-        System.out.print("Baza are: ");
+        System.out.print("Baza are: ");  //todo: english please
         for (int i = 0; i < baza.length; i++) {
             if (i == baza.length - 1) {
                 System.out.print(baza[i] + ".");
