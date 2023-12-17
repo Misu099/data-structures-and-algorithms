482a98c721014b08986525dfd706d8355e45d1a4
Robert24M <95437333+Robert24M@users.noreply.github.com>
2023-12-17 22:04:23 +0200

    feat: add new todo for Misu

--- src/pack/datastructures/MyArrayList.java
+++ src/pack/datastructures/MyArrayList.java
@@ -33,6 +33,8 @@ public class MyArrayList<T> {
 
     public void add(T obj, int index) {
         try {
+            //You are already verifying if the index is out of bounds, so no ArrayIndexOutOfBoundsException can be thrown here
+            //TODO: Remove try catch block and add an else block in witch you throw a new ArrayIndexOutOfBoundsException
             if (index > -1 && index < size) {
                 theArray[index] = obj;
                 size++;
@@ -50,47 +52,44 @@ public class MyArrayList<T> {
         }
     }
 
+    //TODO: Write a get(obj) method and a private search method to see if the array exist in the array and get its index. Use it inside of remove method too
 
     public boolean isEmpty() {
         return size == 0;
     }
 
+    //This will be useless, as it will return true everytime after the initial capacity is reached
     public boolean isFull() {
         return size == theArray.length;
     }
 
     public void remove(int index) {
         if (index > -1 && index < size) {
-            for (int i = index; i < size - 1; i++) {
+            for (int i = index; i < size - 1; i++) { //TODO: extract this in a method because you are using it in 2 places
                 theArray[i] = theArray[i + 1];
             }
             size--;
-            theArray = Arrays.copyOf(theArray, size);
+            theArray = Arrays.copyOf(theArray, size); //TODO: don't need to resize the array wen shrinking, maybe only the differences are big
         }
     }
 
     public void remove(T obj) {
         for (int i = 0; i < theArray.length; i++) {
-            if (theArray[i] != null && theArray[i].equals(obj)) {
-                for (int j = i; j < theArray.length - 1; j++) {
+            if (theArray[i] != null && theArray[i].equals(obj)) { //TODO: replace with theArray[i] == obj. You need to remove the object in case it is null too
+                for (int j = i; j < theArray.length - 1; j++) {  //This is the second place
                     theArray[j] = theArray[j + 1];
                 }
                 size--;
-                theArray = Arrays.copyOf(theArray, size);
+                theArray = Arrays.copyOf(theArray, size); //TODO: don't need to resize the array wen shrinking, maybe only the differences are big
                 break;
-
-
             }
         }
     }
 
-
-    //todo: this method should be rather private and be used just to change the size of array when needed
     private void resize() {
         theArray = Arrays.copyOf(theArray, theArray.length + 1);
     }
 
-    //todo: ugly naming(personal opinion). This may be a toString implementation(read about it or ask me).
     public String toString() {
         StringBuilder stringValue = new StringBuilder("The Array has: ");
         String isFullValue = "It is full: " + isFull() + ". \n";
